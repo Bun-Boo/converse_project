ab7da573dd8030cd11a33adeebb16a03c4079e14
Bun-Boo <studymindx.thanhhai@gmail.com>
2022-05-25 15:49:07 +0700

    sign in , sign up

---
+++ js/signUp.js
@@ -0,0 +1,21 @@
+const btnSignUp = document.getElementById("btn-signup");
+btnSignUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = document.getElementById("username");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const cfpassword = document.getElementById("cfpassword");
+  let user = {
+    username: username.value,
+    email: email.value,
+    password: password.value,
+    cfpass: cfpassword.value,
+  };
+  const json = JSON.stringify(user);
+  if (password.value === cfpassword.value) {
+    localStorage.setItem(username.value, json);
+    alert("Sign Up Success");
+  } else {
+    alert("Passwords are not the same");
+  }
+});
