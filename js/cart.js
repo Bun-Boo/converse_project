152dd207f1220094e7d146588aadcc0113819424
Bun-Boo <studymindx.thanhhai@gmail.com>
2022-06-04 13:53:43 +0700

    add new toast msg click add cart

--- js/cart.js
+++ js/cart.js
@@ -21,6 +21,70 @@ function numberShopping() {
     tolalPro.innerHTML = i + 1;
   }
 }
+
+//-------------Toast Mgs ----------------------//
+function toast({ title = "", message = "", type = "info", duration = 3000 }) {
+  const main = document.getElementById("toast");
+  if (main) {
+    const toast = document.createElement("div");
+
+    // Auto remove toast
+    const autoRemoveId = setTimeout(function () {
+      main.removeChild(toast);
+    }, duration + 1000);
+
+    // Remove toast when clicked
+    toast.onclick = function (e) {
+      if (e.target.closest(".toast__close")) {
+        main.removeChild(toast);
+        clearTimeout(autoRemoveId);
+      }
+    };
+
+    const icons = {
+      success: "fas fa-check-circle",
+      info: "fas fa-info-circle",
+      warning: "fas fa-exclamation-circle",
+      error: "fas fa-exclamation-circle",
+    };
+    const icon = icons[type];
+    const delay = (duration / 1000).toFixed(2);
+
+    toast.classList.add("toast", `toast--${type}`);
+    toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+
+    toast.innerHTML = `
+                      <div class="toast__icon">
+                          <i class="${icon}"></i>
+                      </div>
+                      <div class="toast__body">
+                          <h3 class="toast__title">${title}</h3>
+                          <p class="toast__msg">${message}</p>
+                      </div>
+                      <div class="toast__close">
+                          <i class="fas fa-times"></i>
+                      </div>
+                  `;
+    main.appendChild(toast);
+  }
+}
+
+function successToast() {
+  toast({
+    title: "Thành công!",
+    message: "Sản phẩm đã được thêm vào giỏ hàng.",
+    type: "success",
+    duration: 3000,
+  });
+}
+function WarningToast() {
+  toast({
+    title: "Thông báo!",
+    message: "Sản phẩm đã có trong giỏ hàng, vui lòng cập nhật số lượng.",
+    type: "warning",
+    duration: 3000,
+  });
+}
 //-------------Them san pham ---------------//
 function addCart(productImg, productName, productPrice) {
   let addEletr = document.createElement("tr");
@@ -28,7 +92,7 @@ function addCart(productImg, productName, productPrice) {
   for (let i = 0; i < cartItem.length; i++) {
     let productlistName = document.querySelectorAll(".title");
     if (productlistName[i].innerHTML == productName) {
-      alert("Sản phẩm đã tồn tại ! Vui lòng chọn số lượng sản phẩm");
+      WarningToast();
       return;
     }
   }
@@ -44,6 +108,7 @@ function addCart(productImg, productName, productPrice) {
   addEletr.innerHTML = trContent;
   let cartBody = document.querySelector("tbody");
   cartBody.append(addEletr);
+  successToast();
   totalCart();
   deleteCart();
 }
